795f87927f7077706dc43d52976586803294cfcd
graslowsnail <graslowsnail@gmail.com>
2023-08-20 23:09:00 -0500

    small changed to clear up console

--- app/albums/[albumId]/page.tsx
+++ app/albums/[albumId]/page.tsx
@@ -53,19 +53,19 @@ export default function AlbumDetail({
   params: { pictureId: string};
 }) {
       return (
-           <div className='hero'>
-    <div className='flex-1 pt-20 padding-x'>
-         a list of pictures inside a spcific album
-        <ul role="list" className="grid grid-cols-2 gap-x-3 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8">
-        {files.map((file) => (
-            <li key={file.source} className="relative">
-                <Link href={`/albums/album1/pictures/${file.pictureId}`} passHref>
-                        <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
-                            <img src={file.source} alt="" className="pointer-events-none object-cover group-hover:opacity-75" />
-                            <button type="button" className="absolute inset-0 focus:outline-none">
-                                <span className="sr-only">View details for {file.title}</span>
-                            </button>
-                        </div>
+         <div className='hero'>
+          <div className='flex-1 pt-20 padding-x'>
+           a list of pictures inside a spcific album
+            <ul role="list" className="grid grid-cols-2 gap-x-3 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8">
+              {files.map((file) => (
+                <li key={file.pictureId} className="relative">
+                  <Link href={`/albums/album1/pictures/${file.pictureId}`} passHref>
+                    <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
+                      <img src={file.source} alt="" className="pointer-events-none object-cover group-hover:opacity-75" />
+                        <button type="button" className="absolute inset-0 focus:outline-none">
+                          <span className="sr-only">View details for {file.title}</span>
+                        </button>
+                    </div>
                 </Link>
                 <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">{file.title}</p>
                 <p className="pointer-events-none block text-sm font-medium text-gray-500">{file.size}</p>
--- components/AlbumList.tsx
+++ components/AlbumList.tsx
@@ -55,7 +55,7 @@ const AlbumList = () => {
     a list of albums
         <ul role="list" className="grid grid-cols-2 gap-x-3 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8">
         {files.map((file) => (
-            <li key={file.source} className="relative">
+            <li key={file.albumId} className="relative">
                 <Link href={`/albums/${file.albumId}`} passHref>
                         <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
                             <img src={file.source} alt="" className="pointer-events-none object-cover group-hover:opacity-75" />
