f589fb885ab272811f174fdf8bec286feec7d92e
graslowsnail <graslowsnail@gmail.com>
2023-08-29 16:22:06 -0500

    got single page to fetch picture from lambda

--- app/pictures/[albumId]/[pictureId]/page.tsx
+++ app/pictures/[albumId]/[pictureId]/page.tsx
@@ -1,31 +1,69 @@
-import { testPicture } from '@/constants';
+'use client'
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 
-const picture = testPicture[0]; // Since testPicture is an array, get the first item
+const SimplePicturePage = ({
+  params: { albumId },
+  params: { pictureId },
+}: {
+  params: {
+    albumId: string;
+    pictureId: string;
+  };
+}) => {
+  const [picture, setPicture] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-export function Picture() {
+  useEffect(() => {
+    const fetchData = async () => {
+  try {
+    const response = await fetch(`https://qt2krlwyyg.execute-api.us-east-2.amazonaws.com/picture/${albumId}/${pictureId}`);
+    const data = await response.json();
+    console.log(data);
+    setPicture(data.Item); // Update this line
+    setLoading(false);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    setLoading(false);
+  }
+};
+
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!picture) {
+    return <div>Picture not found.</div>;
+  }
 
   return (
-    <div className='hero'>
-      <div className='flex-1 pt-20 padding-x'>
-        <h1>{picture.title}</h1>
-        <Image 
-          src={picture.imageURL}
-          alt={picture.description}
-          width={500} 
-          height={500}
-          objectFit="cover"
-          className="object-center"
-        />
-        <p>Description: {picture.description}</p>
-        <p>Album: {picture.album_id}</p>
-        <p>Print Size: {picture.printSize}</p>
-        <p>Price: ${picture.price}</p>
-        <p>Status: {picture.isSold ? 'Sold' : 'Available'}</p>
-      </div>
+  <div className="flex flex-col items-center justify-center pt-20 px-6">
+    <h1 className="text-4xl font-bold mb-6">{picture.title}</h1>
+    <div className="relative w-1/2 h-1/2">
+      <Image
+        src={picture.imageURL}
+        alt={`${picture.description}`}
+        width={500}
+        height={500}
+        objectFit="cover"
+        className="rounded-lg object-center"
+      />
     </div>
-  );
-}
+    <div className="mt-6 text-center">
+      <p className="text-lg font-medium mb-2">Description: {picture.description}</p>
+      <p className="text-lg font-medium mb-2">Album: {picture.album_id}</p>
+      <p className="text-lg font-medium mb-2">Print Size: {picture.printSize}</p>
+      <p className="text-lg font-medium mb-2">Price: ${picture.price}</p>
+      <p className={`text-lg font-medium mb-2 ${picture.isSold ? 'text-red-500' : 'text-green-500'}`}>Status: {picture.isSold ? 'Sold' : 'Available'}</p>
+    </div>
+  </div>
 
-export default Picture;
+  );
+};
+
+export default SimplePicturePage;
 
