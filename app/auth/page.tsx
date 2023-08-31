bca8c0fa7caace922b3ff2ea82788a710252bf59
graslowsnail <graslowsnail@gmail.com>
2023-08-31 13:46:22 -0500

    added comments to the api, deleted random code in the auth page

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,11 +1,9 @@
-import { Checkout } from '@/components';
 
 export function AuthPage() {
   return (
        <div className='hero'>
     <div className='flex-1 pt-20 padding-x'>
       <h1>auth page</h1>
-      <Checkout/>
       </div>
       </div>
   );
