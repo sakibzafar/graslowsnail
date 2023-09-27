c47f63dbb6086a4ee026a07fc211b1bd22f4ac19
graslowsnail <graslowsnail@gmail.com>
2023-09-26 19:56:47 -0500

    added v2 captcha to the contact form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,18 @@
 // pages/faq.tsx
 'use client'
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm, ValidationError } from '@formspree/react';
+import Head from 'next/head';
+import ReCAPTCHA from 'react-google-recaptcha';
 
-export default function FAQ() {
+export default function CONTACT() {
   const [state, handleSubmit] = useForm("mknlqlgq");
+  const [captchaValue, setCaptchaValue] = useState(null);
+
+   const onCaptchaChange = (value) => {
+    setCaptchaValue(value);
+  };
 
   if (state.succeeded) {
       return(
@@ -18,51 +25,58 @@ export default function FAQ() {
   }
 
   return (
-    <div className="flex justify-center items-center min-h-screen ">
-      <form onSubmit={handleSubmit} className="w-full max-w-md bg-gray-100 p-6 rounded-xl shadow-md">
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-sm font-medium text-gray-600 mb-2">
-            Email Address
-          </label>
-          <input
-            id="email"
-            type="email" 
-            name="email"
-            className="w-full p-2 border rounded-md"
-          />
-          <ValidationError 
-            prefix="Email" 
-            field="email"
-            errors={state.errors}
-            className="text-red-600 text-xs mt-1"
-          />
-        </div>
-        
-        <div className="mb-4">
-          <label htmlFor="message" className="block text-sm font-medium text-gray-600 mb-2">
-            Message
-          </label>
-          <textarea
-            id="message"
-            name="message"
-            rows={4}
-            className="w-full p-2 border rounded-md"
-          />
-          <ValidationError 
-            prefix="Message" 
-            field="message"
-            errors={state.errors}
-            className="text-red-600 text-xs mt-1"
-          />
-        </div>
+      <div className="flex justify-center items-center min-h-screen ">
+        <form onSubmit={handleSubmit} className="w-full max-w-md bg-gray-100 p-6 rounded-xl shadow-md">
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-sm font-medium text-gray-600 mb-2">
+              Email Address
+            </label>
+            <input
+              id="email"
+              type="email" 
+              name="email"
+              className="w-full p-2 border rounded-md"
+            />
+            <ValidationError 
+              prefix="Email" 
+              field="email"
+              errors={state.errors}
+              className="text-red-600 text-xs mt-1"
+            />
+          </div>
+          
+          <div className="mb-4">
+            <label htmlFor="message" className="block text-sm font-medium text-gray-600 mb-2">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              rows={4}
+              className="w-full p-2 border rounded-md"
+            />
+            <ValidationError 
+              prefix="Message" 
+              field="message"
+              errors={state.errors}
+              className="text-red-600 text-xs mt-1"
+            />
+          </div>
 
-        <div className="flex justify-end">
-          <button type="submit" disabled={state.submitting} className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md">
-            Submit
-          </button>
-        </div>
-      </form>
-    </div>
+          {/* Add reCAPTCHA */}
+          <ReCAPTCHA
+            sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+            onChange={onCaptchaChange}
+          />
+
+          <div className="flex justify-end">
+            <button type="submit" disabled={state.submitting || !captchaValue} className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+
   );
 }
 
